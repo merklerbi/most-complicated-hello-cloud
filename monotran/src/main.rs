642b159803b1a890e96609a4d666cefe081f6991
Alexander Schaub <alexander.schaub@helixton.de>
2023-03-22 15:57:34 +0100

    monotran > update > integrated reqwest api call for getting response as json

--- monotran/src/main.rs
+++ monotran/src/main.rs
@@ -1,4 +1,19 @@
+use std::collections::HashMap;
+
 use error_chain::error_chain;
+use serde::{Serialize, Deserialize};
+
+use reqwest::header::CONTENT_TYPE;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct GETAPIResponse {
+    origin: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct JSONResponse {
+    json: HashMap<String, String>,
+}
 
 error_chain! {
     foreign_links {
@@ -9,13 +24,13 @@ error_chain! {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let res = reqwest::get("http://worldtimeapi.org/api/timezone/Europe/Berlin.txt").await?;
-    // println!("Status: {}", res.status());
-    // println!("Headers:\n{:#?}", res.headers());
-
-    let body = res.text().await?;
-
+    let client = reqwest::Client::new();
+    let res = client.get("http://worldtimeapi.org/api/timezone/Europe/Berlin")
+        .header(CONTENT_TYPE, "application/json")
+        .send()
+        .await?
+        .json::<GETAPIResponse>()
+        .await?;
     
-    println!("Body:\n{}", body);
     Ok(())
 }
